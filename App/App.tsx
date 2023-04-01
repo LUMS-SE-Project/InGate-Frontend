900f6e9c8765b1ad3aa4aa12a55865821b588f4e
KabirK71 <kabirkateja71@gmail.com>
2023-04-01 22:33:40 +0500

    developed the login page

--- App/App.tsx
+++ App/App.tsx
@@ -5,10 +5,7 @@
  * @format
  */
 
-import {
-  useState,
-  Component
-} from 'react';
+import {useState, Component} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -19,55 +16,40 @@ import {
   StyleSheet,
   AppRegistry,
 } from 'react-native';
-import { 
-  Button 
-} from 'react-native-elements';
-import {
-  NavigationContainer
-} from '@react-navigation/native';
-import { 
-  createNativeStackNavigator 
-} from '@react-navigation/native-stack';
+import {Button} from 'react-native-elements';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import LandingPage from './src/screens/LandingPage';
 import LoginPage from './src/screens/LoginPage';
 
-
 const Stack = createNativeStackNavigator();
 
-
-
 function App(): JSX.Element {
-  
-
   return (
     <NavigationContainer>
-      <SafeAreaView className='bg-[#3D63FF]' />
-      <SafeAreaView className='min-h-screen min-w-screen flex justify-center align-middle bg-white'>
-      
-        <Stack.Navigator initialRouteName='LandingPage'>
-          <Stack.Screen 
-            name="LandingPage" 
+      <SafeAreaView className="bg-[#3D63FF]" />
+      <SafeAreaView className="min-h-screen min-w-screen flex justify-center align-middle bg-white">
+        <Stack.Navigator initialRouteName="LandingPage">
+          <Stack.Screen
+            name="LandingPage"
             component={LandingPage}
             options={{
               headerShown: false,
             }}
-            />
+          />
 
           <Stack.Screen
-            name="LoginPage" 
-            component={LoginPage} 
-            options = {{
+            name="LoginPage"
+            component={LoginPage}
+            options={{
               headerShown: false,
             }}
-            />
+          />
         </Stack.Navigator>
-      
       </SafeAreaView>
     </NavigationContainer>
   );
 }
-
-
 
 export default App;
--- App/src/screens/LandingPage.tsx
+++ App/src/screens/LandingPage.tsx
@@ -1,27 +1,23 @@
-import React from "react";
-import {
-    Text,
-    View,
-    ImageBackground,
-} from "react-native";
+import React from 'react';
+import {Text, View, ImageBackground} from 'react-native';
 
+const LandingPage = ({navigation}): JSX.Element => {
+  setTimeout(() => {
+    navigation.navigate('LoginPage');
+  }, 2000);
 
-const LandingPage = ({ navigation }) : JSX.Element=> {
-    
-    setTimeout(()=>{
-        navigation.navigate('LoginPage')
-    }, 2000)
-
-    return(
-
-        <View className='min-h-screen min-w-screen flex justify-center align-center'>
-            <ImageBackground source={require("../images/GradientBackground.png")} resizeMode='stretch' className='min-h-screen min-w-screen flex justify-center align-middle' >
-              <Text className='text-8xl font-Montserrat text-center text-white mt-[-100px]'>InGate</Text>
-            </ImageBackground>
-        </View>
-    
-    )
-
+  return (
+    <View className="min-h-screen min-w-screen flex justify-center align-center">
+      <ImageBackground
+        source={require('../images/GradientBackground.png')}
+        resizeMode="stretch"
+        className="min-h-screen min-w-screen flex justify-center align-middle">
+        <Text className="text-8xl font-Montserrat text-center text-white mt-[-100px]">
+          InGate
+        </Text>
+      </ImageBackground>
+    </View>
+  );
 };
 
 export default LandingPage;
