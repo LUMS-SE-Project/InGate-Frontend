741c2b85920cb59fcb6079bf60036f40377ade99
KabirK71 <kabirkateja71@gmail.com>
2023-04-07 21:55:14 +0500

    Khareedar Order Details page 1 and 2 made

--- App/App.tsx
+++ App/App.tsx
@@ -25,7 +25,8 @@ import LandingPage from './src/screens/LandingPage';
 import LoginPage from './src/screens/LoginPage';
 import SignUp1 from './src/screens/SignUp1';
 import SignUp2 from './src/screens/SignUp2';
-
+import KhareedarOrderDetails1 from './src/screens/KhareedarOrderDetails1';
+import KhareedarOrderDetails2 from './src/screens/KhareedarOrderDetails2';
 import RequestItem from './src/screens/RequestItemPage';
 import ItemsPage from './src/screens/Items';
 
@@ -36,7 +37,7 @@ function App(): JSX.Element {
     <NavigationContainer>
       <SafeAreaView className="bg-[#3D63FF]" />
       <SafeAreaView className="min-h-screen min-w-screen flex justify-center align-middle bg-white">
-        <Stack.Navigator initialRouteName="ItemsPage">
+        <Stack.Navigator initialRouteName="KhareedarOrderDetails2">
           <Stack.Screen
             name="LandingPage"
             component={LandingPage}
@@ -78,6 +79,22 @@ function App(): JSX.Element {
           <Stack.Screen
             name="ItemsPage"
             component={ItemsPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+
+          <Stack.Screen
+            name="KhareedarOrderDetails1"
+            component={KhareedarOrderDetails1}
+            options={{
+              headerShown: false,
+            }}
+          />
+
+          <Stack.Screen
+            name="KhareedarOrderDetails2"
+            component={KhareedarOrderDetails2}
             options={{
               headerShown: false,
             }}
