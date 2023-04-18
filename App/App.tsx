6cb3cd6fabdc951af68f8b377239874da44f01ab
KabirK71 <kabirkateja71@gmail.com>
2023-04-18 21:53:23 +0500

    minor changes

--- App/App.tsx
+++ App/App.tsx
@@ -20,7 +20,7 @@ function App(): JSX.Element {
     <NavigationContainer>
       <SafeAreaView className="bg-[#3D63FF]" />
       <SafeAreaView className="min-h-screen min-w-screen flex justify-center align-middle bg-white">
-        <Stack.Navigator initialRouteName="LoginPage">
+        <Stack.Navigator initialRouteName="ItemsPage">
           <Stack.Screen
             name="LandingPage"
             component={LandingPage}
