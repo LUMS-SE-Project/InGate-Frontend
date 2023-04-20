95370a18381d78b2e1a82ca8bf469698baea6452
KabirK71 <kabirkateja71@gmail.com>
2023-04-20 22:21:18 +0500

    kabir changes 2

--- App/src/navigate/UserNavigate.tsx
+++ App/src/navigate/UserNavigate.tsx
@@ -3,8 +3,10 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Items from '../screens/Items';
 
 import DostOrders from '../screens/DostOrders';
-
-
+import RequestItem from '../screens/RequestItemPage';
+import PartialOrder from '../screens/PartialOrder';
+import KhareedarOrderDetails1 from '../screens/KhareedarOrderDetails1';
+import KhareedarOrderDetails2 from '../screens/KhareedarOrderDetails2';
 export const UserNavigate = () => {
   const Stack = createNativeStackNavigator();
 
@@ -17,6 +19,16 @@ export const UserNavigate = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Items" component={Items} />
+      <Stack.Screen name="RequestItem" component={RequestItem} />
+      <Stack.Screen name="PartialOrder" component={PartialOrder} />
+      <Stack.Screen
+        name="KhareedarOrderDetails1"
+        component={KhareedarOrderDetails1}
+      />
+      <Stack.Screen
+        name="KhareedarOrderDetails2"
+        component={KhareedarOrderDetails2}
+      />
     </Stack.Navigator>
   );
 };
