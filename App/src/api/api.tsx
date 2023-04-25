760b6fb2351ba97cdc1f0e1be83aea6d2876a864
Muhammad Jazlan <ja.zl.an@live.com>
2023-04-26 02:21:25 +0500

    Fixed android API issues

--- App/src/api/api.tsx
+++ App/src/api/api.tsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'http://139.59.59.116/',
   headers: {
-    'Content-Type' : 'multipart/form-data',
     'Accept' : 'application/json'
   },
 });
